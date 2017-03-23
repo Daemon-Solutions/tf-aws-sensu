a6e431b778f4656b643a91bfdfcc53ede8accc06
Steven Datt <steven.datt@bashton.com>
2017-03-23 13:05:05 +0000

    added extra variables for configuration

--- elasticache.tf
+++ elasticache.tf
@@ -6,7 +6,9 @@ module "sensu-elasticache" {
   subnets     = "${var.private_subnets}"
   vpc_id      = "${var.vpc_id}"
   vpc_cidr    = "${var.vpc_cidr}"
-  instance_type = "${var.elasticache_instance_type}"
+  node_type = "${var.elasticache_instance_type}"
+  parameter_group_name = "${var.parameter_group_name}"
+  engine_version = "${var.engine_version}"
   snapshot_window = "${var.snapshot_window}"
   snapshot_retention_limit = "${var.snapshot_retention_limit}"
 }
