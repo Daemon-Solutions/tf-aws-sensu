7126ce7f09b9d92883f2d135f4aa3b2393e22226
steven.datt <steven.datt@bashton.com>
2017-03-28 16:56:03 +0100

    corrected some output names

--- outputs.tf
+++ outputs.tf
@@ -1,31 +1,23 @@
 # Output endpoints for elasticache
 
-output "sensu_instance_type" {
-	value = "${sensu_instance_type}"
-}
-output "sensu_sg_id" {
-  value = "${aws_security_group.sensu.id}"
-}
-
 output "endpoint" {
-  value = "${module.elasticache.cache_nodes}"
+  value = "${module.sensu-elasticache.cache_nodes}"
 }
 
 output "node_address" {
-  value = "${module.elasticache.cache_nodes.0.address}"
+  value = "${module.sensu-elasticache.cache_nodes.0.address}"
 }
 
 output "memcached_endpoint" {
-  value = "${module.elasticache.configuration_endpoint}"
+  value = "${module.sensu-elasticache.configuration_endpoint}"
 }
-
 
 # Output endpoints for elasticache-repgroup
 
 output "endpoint" {
-  value = "${module.elasticache-repgroup.sensu.endpoint}"
+  value = "${module.elasticache-rep-group.sensu.endpoint}"
 }
 
 output "configuration_endpoint" {
-  value = "${module.elasticache-repgroup.configuration_endpoint}"
+  value = "${module.elasticache-rep-group.configuration_endpoint}"
 }
