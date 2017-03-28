9378a4c4acf8aecdd1921a575cc7ee9ed278ddb9
steven.datt <steven.datt@bashton.com>
2017-03-28 16:53:19 +0100

    added additional outputs, depending on which type of elasticache is used

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,31 @@
-# Outputs
+# Output endpoints for elasticache
 
 output "sensu_instance_type" {
 	value = "${sensu_instance_type}"
 }
 output "sensu_sg_id" {
   value = "${aws_security_group.sensu.id}"
+}
+
+output "endpoint" {
+  value = "${module.elasticache.cache_nodes}"
+}
+
+output "node_address" {
+  value = "${module.elasticache.cache_nodes.0.address}"
+}
+
+output "memcached_endpoint" {
+  value = "${module.elasticache.configuration_endpoint}"
+}
+
+
+# Output endpoints for elasticache-repgroup
+
+output "endpoint" {
+  value = "${module.elasticache-repgroup.sensu.endpoint}"
+}
+
+output "configuration_endpoint" {
+  value = "${module.elasticache-repgroup.configuration_endpoint}"
 }
