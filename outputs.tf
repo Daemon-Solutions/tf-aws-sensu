3577e4679bda8a2ff0c5e3f0a61ece6dc9c28a3f
steven.datt <steven.datt@bashton.com>
2017-03-29 13:12:26 +0100

    testing new outputs

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,23 @@
 # Output endpoints for elasticache
 
-output "endpoint" {
+output "elasticache_cluster_redis_endpoint" {
   value = "${module.sensu-elasticache.cache_nodes}"
 }
 
-output "node_address" {
+output "elasticache_cluster_node_address" {
   value = "${module.sensu-elasticache.cache_nodes.0.address}"
 }
 
-output "memcached_endpoint" {
+output "elasticache_cluster_memcached_endpoint" {
   value = "${module.sensu-elasticache.configuration_endpoint}"
 }
 
 # Output endpoints for elasticache-repgroup
 
-output "endpoint" {
-  value = "${module.sensu-elasticache.endpoint}"
+output "elasticache_replication_group_endpoint" {
+  value = "${module.sensu-elasticache.elasticache_replication_group_endpoint}"
 }
 
-output "configuration_endpoint" {
-  value = "${module.sensu-elasticache.configuration_endpoint}"
+output "elasticache_replication_group_configuration_endpoint" {
+  value = "${module.sensu-elasticache.elasticache_replication_group_configuration_endpoint}"
 }
